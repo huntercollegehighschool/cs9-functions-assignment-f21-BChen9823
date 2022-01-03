9d3171ed2c501cfbbfb2548e48fd26f125227242
Billy Chen <93930054+BChen9823@users.noreply.github.com>
2022-01-03 22:57:21 +0000

    Part1.py(Fixed Code)

--- part1.py
+++ part1.py
@@ -2,6 +2,7 @@
 ******
 PART 1
 ******
+
 The function distinct() below takes three numbers as arguments, and returns True (Boolean value, not the string, so no quotes necessary) if the three numbers are all different, and False (Boolean value, not the string, so no quotes necessary) if not.
 
 However, there are (at least) 7 errors in the code. Fix them so that it runs properly.
